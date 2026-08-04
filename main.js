7ba4ebcba8484c0724b12615c6dc4dfff4009639
Moyou <newvunus1234.com@gmail.com>
2026-08-04 09:38:57 +0700

    add section and product

--- main.js
+++ main.js
@@ -1,17 +1,58 @@
-const product = {
-    id: 0,
-    Image: "Moto-collection/icon/map.svg",
-    price: 180
-};
+const products = [
+    {
+        id: 1,
+        name: "Test",
+        image: "https://placehold.co/300x200",
+        price: 180,
+        stock: 20
+    },
 
-const producttlist = document.getElementById("#productlist");
+    {
+        id: 2,
+        name: "Test",
+        image: "https://placehold.co/300x200",
+        price: 150,
+        stock: 15
+    },
 
-productlist.innerHTML = `
-    <div class="card">
-        <img src="${product.image}" alt="produ">
-        <p>ID: ${product.id}</p>
-        <p>Price: ${product.price} บาท</p>
-    </div>
-`;
+    {
+        id: 3,
+        name: "Test",
+        image: "https://placehold.co/300x200",
+        price: 200,
+        stock: 10
+    },
 
+    {
+        id: 4,
+        name: "Test",
+        image: "https://placehold.co/300x200",
+        price: 200,
+        stock: 10
+    }
+];
+
+const productlist = document.querySelector("#productlist");
+
+products.forEach(product => {
+
+    productlist.innerHTML += `
+        <div class ="container">
+        <div class="card">
+        <div class ="box-pd">
+            <img src="${product.image}" alt="${product.name}">
+
+            <h2>${product.name}</h2>
+
+            <div class = "textend">            
+            <p>฿${product.price}</p>
+            <p>เหลือ ${product.stock} ชิ้น</p>
+            </div>
+        </div>
+        </div>
+        </div>
+    `;
+
+});
 console.log(product);
+console.log(product.image);
